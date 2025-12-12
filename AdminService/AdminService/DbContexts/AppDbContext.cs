322935e046bcf0a3a40cfca5d0925a560b27b98e
Abd El-Rahman Ali <abdel292004@gmail.com>
2025-12-12 21:55:39 +0200

    Add models and context for admin

---
+++ AdminService/AdminService/DbContexts/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AdminService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace AdminService.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=DefaultConnection") { }
+
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<CartProduct> Cart_Products { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderProduct> Order_Products { get; set; }
+        public DbSet<FAQ> FAQ { get; set; }
+    }
+}
